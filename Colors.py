2c5085fb618b04febfcf89fe2499058d5976f7af
Jared J <26422790+ipaqmaster@users.noreply.github.com>
2026-01-06 10:38:46 +1100

    Add loglevel + logger support in printer() of Colors.py

--- Colors.py
+++ Colors.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 # An array of ANSI colors I like to use
 colors =  {
@@ -13,19 +14,40 @@ colors =  {
   "white": "\033[97m"
 }
 
-def printer(string='', end=None):
-    "A print wrapper which tries to add colors"
+def printer(message='', message_loglevel=0, loglevel=0, indent=0, end=None, logger=None):
+    "A print wrapper which tries to add colors, indent and handle a loglevel"
+    message_original    = message
+    should_print        = None
+    colorProcessing     = False
 
-    colorProcessing = False
-    if type(string) == str:
-      if '%' in string:
-          colorEscapeScan = re.findall(r'%[a-zA-Z]+%', string)
-          for colorEscape in colorEscapeScan:
-              color = colorEscape.strip('%').lower()
-              if color in colors:
-                  string = string.replace(colorEscape, colors[color])
-                  colorProcessing = True
+    indents = ' ' * 4 * indent
+    file=sys.stdout
+
+    # Skip printing if the log level is higher than the message
+    if message_loglevel > loglevel:
+        should_print = False
+    else:
+        should_print = True
+        # Use stderr for any log level higher than 0
+        if loglevel > 0:
+            file=sys.stderr
+
+    # Replace color placeholders with ANSI color escape sequences
+    if type(message) == str:
+        if '%' in message:
+            colorEscapeScan = re.findall(r'%[a-zA-Z]+%', message)
+            for colorEscape in colorEscapeScan:
+                color = colorEscape.strip('%').lower()
+                if color in colors:
+                    message = message.replace(colorEscape, colors[color])
+                    colorProcessing = True
 
     if colorProcessing:
-        string = string + colors['none']
-    print(string, end=None)
+        message = message + colors['none'] # Switch color back off at the end of any message.
+
+    # Print
+    print(message, end=None)
+
+    # If logger is set, log too.
+    if logger:
+        logger(message_original)
